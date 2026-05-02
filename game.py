2ee66d572ec8d944fc21cd6ce0554d67c194a26a
Habibulloh <habibulloh@example.com>
2026-05-02 18:01:25 +0500

    Upgrade
        rename: main.py to main.py

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ secret_number = randint(1, 10)
 while True:
     guess = int(input("Guess the number between 1 and 10: "))
     if guess == secret_number:
-        print("Congratulations! You guessed the number.")
+        print("Congratulations! You guessed the number✅.")
         break
     else:
         print("Wrong guess. Try again!")
