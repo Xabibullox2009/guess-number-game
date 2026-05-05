abbe246032ee045628193e6b2022628fdb1f2851
khabib <habibulloh@example.com>
2026-05-05 18:31:45 +0500

    Attempts adding

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
 from random import randint
 
-count = 1
 secret_number = randint(1, 10)
-while True:
+attempts = 3
+
+while attempts > 0:
     guess = int(input("Guess the number between 1 and 10: "))
-    count += 1
-    if count >=3:
-        print("Game over! You've used all your attempts. The secret number was:", secret_number)
-        break
+    attempts -= 1
 
     if guess == secret_number:
-        print("Congratulations! You guessed the number✅.")
+        print("Congratulations! You guessed the number ✅")
         break
-    if guess < secret_number:
+    elif guess < secret_number:
         print("Too low! Try again!")
-    elif guess > secret_number:
-        print("Too high! Try again!")
     else:
-        print("Wrong guess❌. Try again!")
-        
+        print("Too high! Try again!")
+
+    if attempts > 0:
+        print(f"You have {attempts} attempts left.")
+    else:
+        print("Game over! You've used all your attempts.")
+        print(f"The number was: {secret_number}")
